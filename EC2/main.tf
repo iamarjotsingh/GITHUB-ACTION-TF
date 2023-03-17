1eb95f3ae05fe0a4ff047a15a5586711596b78f7
amar1452001 <amarjotsingh5555@gmail.com>
2023-03-17 12:39:08 +0530

    terraform workflow

--- EC2/main.tf
+++ EC2/main.tf
@@ -1,13 +1,13 @@
 resource "aws_instance" "AmarEC2" {
-  ami           = "ami-0d81306eddc614a45"
-  instance_type = "t2.micro"
+    ami           = "ami-0d81306eddc614a45"
+    instance_type = "t2.micro"
 
-  tags = {
-    Name = "Amarjot Singh"
-    Owner = "amarjot.singh@cloudeq.com"
-  }
-  volume_tags = {
-    "Name" = "Amarjot Singh"
-    "Owner" = "amarjot.singh@cloudeq.com"
-  }
+    tags = {
+        Name = "Amarjot Singh"
+        Owner = "amarjot.singh@cloudeq.com"
+    }
+    volume_tags = {
+        "Name" = "Amarjot Singh"
+        "Owner" = "amarjot.singh@cloudeq.com"
+    }
 }
